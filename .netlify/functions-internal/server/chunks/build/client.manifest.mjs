a422c17f23b2642a14a12b4f452723de85cc3f6e
Nicolas marcaud <nmarcau@gmail.com>
2025-02-11 18:03:07 +0100

    Build

---
+++ .netlify/functions-internal/server/chunks/build/client.manifest.mjs
@@ -0,0 +1,146 @@
+const client_manifest = {
+  "_BwLaerhi.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BwLaerhi.js",
+    "name": "useCourse"
+  },
+  "_C6N-MMxg.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C6N-MMxg.js",
+    "name": "nuxt-link",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_KLCb6Rqj.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "KLCb6Rqj.js",
+    "name": "vue.-sixQ7xP",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/nuxt/dist/app/components/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DDsKvbFR.js",
+    "name": "error-404",
+    "src": "node_modules/nuxt/dist/app/components/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_C6N-MMxg.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_KLCb6Rqj.js"
+    ],
+    "css": []
+  },
+  "error-404.C3V-3Mc4.css": {
+    "file": "error-404.C3V-3Mc4.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/components/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "5yfGLsTI.js",
+    "name": "error-500",
+    "src": "node_modules/nuxt/dist/app/components/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_KLCb6Rqj.js"
+    ],
+    "css": []
+  },
+  "error-500.dGVH929u.css": {
+    "file": "error-500.dGVH929u.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BCKpOgZl.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "node_modules/nuxt/dist/app/components/error-404.vue",
+      "node_modules/nuxt/dist/app/components/error-500.vue"
+    ],
+    "_globalCSS": true
+  },
+  "pages/course.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DrJjm5vM.js",
+    "name": "course",
+    "src": "pages/course.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_C6N-MMxg.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_BwLaerhi.js"
+    ]
+  },
+  "pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "B-AcBqAD.js",
+    "name": "_lessonSlug_",
+    "src": "pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_C6N-MMxg.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_BwLaerhi.js",
+      "_KLCb6Rqj.js"
+    ],
+    "css": [
+      "_lessonSlug_.Bipjk9Zd.css"
+    ]
+  },
+  "_lessonSlug_.Bipjk9Zd.css": {
+    "file": "_lessonSlug_.Bipjk9Zd.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "tAVBaCZs.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
