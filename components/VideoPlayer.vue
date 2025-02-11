f6acbae92f11ebf269df3e769d219ab8cf279870
Nicolas marcaud <nmarcau@gmail.com>
2025-02-11 15:43:38 +0100

    1-7 Add VideoPlayer Component

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,19 @@
+<template>
+  <iframe
+    width="560"
+    height="315"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    title="Video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+</template>
+
+<script setup>
+type Props = {
+  videoId: string;
+};
+
+const props = defineProps<Props>();
+</script>
