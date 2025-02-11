f2ed721345c36b6dbc11449d09161978a4802f19
Nicolas marcaud <nmarcau@gmail.com>
2025-02-11 15:35:57 +0100

    1-5 Loading in Course Data

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -4,7 +4,26 @@
     >
     <h2>Lesson</h2>
     <p>This is a lesson</p>
-    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
-    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+    <p>{{ chapter.title }}</p>
+    <p>{{ lesson.title }}</p>
+
+    <p>{{ lesson.text }}</p>
   </div>
 </template>
+
+<script setup>
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find((chapter) => {
+    return chapter.slug === route.params.chapterSlug;
+  });
+});
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find((lesson) => {
+    return lesson.slug === route.params.lessonSlug;
+  });
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,1 @@
-<template>
-  <div>
-    <h1>Hello World</h1>
-    <p>Click <nuxt-link to="/about">here</nuxt-link> to go to the about page</p>
-  </div>
-</template>
+<template>Hello, world!</template>
