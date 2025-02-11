6672845dafee363192998884cc7ce46e9ee125c2
Nicolas marcaud <nmarcau@gmail.com>
2025-02-11 15:14:53 +0100

    1-4 Dynamic Routes

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -4,5 +4,7 @@
     >
     <h2>Lesson</h2>
     <p>This is a lesson</p>
+    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
+    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
   </div>
 </template>
