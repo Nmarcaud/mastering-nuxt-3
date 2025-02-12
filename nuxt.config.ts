1612d3678ea95acd4ba70325fae41df94d8a6311
Nicolas marcaud <nmarcau@gmail.com>
2025-02-12 08:36:21 +0100

    2-3 Understanding Layouts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+
   nitro: {
     preset: 'netlify'
-  }
+  },
+
+  compatibilityDate: '2025-02-11'
 })
